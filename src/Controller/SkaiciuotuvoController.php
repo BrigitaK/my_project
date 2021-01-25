cf83293f713bbb968e8880af08591c87a81d265f
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2021-01-25 17:06:40 +0200

    skaiciuotuvas

--- src/Controller/SkaiciuotuvoController.php
+++ src/Controller/SkaiciuotuvoController.php
@@ -8,8 +8,62 @@ class SkaiciuotuvoController extends AbstractController
 {
     public function suma(int $num1, int $num2)
     {
+        $dalybaUrl = $this->generateUrl('dalyba', ['num1' => $num1, 'num2' => $num2]);
+        $daugybaUrl = $this->generateUrl('daugyba', ['num1' => $num1, 'num2' => $num2]);
+        $atimtisUrl = $this->generateUrl('atimtis', ['num1' => $num1, 'num2' => $num2]);
+
         return $this->render('skaiciuoti/suma.html.twig',[
-            'sum' => $num1 + $num2
+            'sum' => $num1 + $num2,
+            'num1' => $num1,
+            'num2' => $num2,
+            'goDal' => $dalybaUrl,
+            'goDaug' => $daugybaUrl,
+            'goAti' => $atimtisUrl
+        ]);
+    }
+    public function dalyba(int $num1, int $num2)
+    {
+        $sumaUrl = $this->generateUrl('suma', ['num1' => $num1, 'num2' => $num2]);
+        $daugybaUrl = $this->generateUrl('daugyba', ['num1' => $num1, 'num2' => $num2]);
+        $atimtisUrl = $this->generateUrl('atimtis', ['num1' => $num1, 'num2' => $num2]);
+
+        return $this->render('skaiciuoti/dalyba.html.twig',[
+            'dal' => $num1 / $num2,
+            'num1' => $num1,
+            'num2' => $num2,
+            'goSum' => $sumaUrl,
+            'goDaug' => $daugybaUrl,
+            'goAti' => $atimtisUrl
+        ]);
+    }
+    public function daugyba(int $num1, int $num2)
+    {
+        $sumaUrl = $this->generateUrl('suma', ['num1' => $num1, 'num2' => $num2]);
+        $dalybaUrl = $this->generateUrl('dalyba', ['num1' => $num1, 'num2' => $num2]);
+        $atimtisUrl = $this->generateUrl('atimtis', ['num1' => $num1, 'num2' => $num2]);
+
+        return $this->render('skaiciuoti/daugyba.html.twig',[
+            'daug' => $num1 * $num2,
+            'num1' => $num1,
+            'num2' => $num2,
+            'goSum' => $sumaUrl,
+            'goDal' => $dalybaUrl,
+            'goAti' => $atimtisUrl
+        ]);
+    }
+    public function atimtis(int $num1, int $num2)
+    {
+        $sumaUrl = $this->generateUrl('suma', ['num1' => $num1, 'num2' => $num2]);
+        $dalybaUrl = $this->generateUrl('dalyba', ['num1' => $num1, 'num2' => $num2]);
+        $daugybaUrl = $this->generateUrl('daugyba', ['num1' => $num1, 'num2' => $num2]);
+
+        return $this->render('skaiciuoti/atimtis.html.twig',[
+            'ati' => $num1 - $num2,
+            'num1' => $num1,
+            'num2' => $num2,
+            'goSum' => $sumaUrl,
+            'goDal' => $dalybaUrl,
+            'goDaug' => $daugybaUrl
         ]);
     }
 }
