29d6984a73c0910e3cd6eaf2a09b13e34af6b9cc
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2021-01-26 23:57:15 +0200

    calculator

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -11,28 +11,30 @@ use Symfony\Component\HttpFoundation\Session\Session;
 class CalculatorController extends AbstractController
 {
     /**
-     * @Route("/calculator", name="calculator")
+     * @Route("/calculator", name="calculator", methods={"GET"})
      */
-    #[Route('/calculator', name: 'calculator', methods: ['GET'])]
     public function index(): Response
     {
         $session = new Session();
-        $session->start();
+        $result = $session->getFlashBag()->get('result', []);
+
         return $this->render('calculator/index.html.twig', [
             'controller_name' => 'CalculatorController',
+            'result' => $result[0] ?? ''
         ]);
     }
     /**
-     * @Route("/calculator", name="count")
+     * @Route("/calculator", name="count", methods={"POST"})
      */
-    #[Route('/calculator', name: 'count', methods: ['POST'])]
     public function counter(Request $r): Response
     {
         $session = new Session();
-        $session->start();
         $sum = $r->request->get('x') + $r->request->get('y');
 
-        $session->set('result', $sum);
+        // $session->set('result', $sum);
+        $session->getFlashBag()->add('result', $sum);
+
+        return $this->redirectToRoute('calculator');
         
 
     }
